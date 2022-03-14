a3a8191b0ad5fb0c804227221ee27a5e34636b40
hassanamg95 <amgharid.hassan2@ofppt-edu.ma>
2022-03-14 15:07:11 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true , limit: '10kb'}))
 app.use('/', clientRouter)
 app.use('/', downloadRoute)
 
-app.use('/',  (req, res) => {
+app.use('',  (req, res) => {
 
     res.render('index')
 
